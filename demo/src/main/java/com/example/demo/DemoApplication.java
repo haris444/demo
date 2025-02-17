b3d389680a37d5bf096e778d092f06c42a337094
unknown <haris444.hv@gmail.com>
2025-02-17 15:05:32 +0200

    Version 1

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Version 1.0
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//Version 1.1
+		System.out.println("V1.1");
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+
+
+}
